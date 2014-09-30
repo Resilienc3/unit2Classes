7909d0aa14069a185ba59e3526961841391deac8
Resilienc3 <jsmitty1414@gmail.com>
2014-09-30 12:29:25 -0500

    practice

--- Chapter 4 Individual Assignments/CashRegister.java
+++ Chapter 4 Individual Assignments/CashRegister.java
@@ -5,10 +5,29 @@ public class CashRegister
     public static void main (String[] args)
     {
         // Scanned owed and received
-        //int amountOwed;
-        //int amountReceived;
-        //int changeDue = amountReceived - amountOwed; 
+        int amountOwed= 1000;
+        int amountPaid = 2000;
+        int changeDue = amountPaid - amountOwed;
+        final int QUARTER_V = 25;
+        final int DIME_V = 10;
+        final int NICKEL_V = 5;
+        int qNum = 0;
+        int dNum = 0;
+        int nNum = 0;
+        int pNum = 0;
+        int numC = changeDue;
+        qNum = numC/QUARTER_V;
+        numC = changeDue - (numC/QUARTER_V);
+        int mod = numC%QUARTER_V;
+        dNum = mod/DIME_V;
+        numC = numC - (numC/DIME_V);
+        mod = numC%DIME_V;
+        nNum = mod/NICKEL_V;
+        numC = numC - (numC/NICKEL_V);
+        mod = numC%NICKEL_V;
+        pNum = mod; 
         
+        System.out.println("Quarters: " + qNum +" Dimes: " + dNum +" Nickels: "+ nNum +" Pennies: " + pNum);
     }
 }
 // Divide by quarters, mod by quarters, while mod != 0 divide by dimes, mod by dimes, etc.
